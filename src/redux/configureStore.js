e0cae9060ade5c5cf1bdf1de0de13eef5787824a
normainobary2021 <norman.ainobushoborozi@gmail.com>
2022-11-11 11:22:18 +0300

    Create store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,13 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './Books/Books';
 import categoriesReducer from './Categories/Categories';
 
-const reducers = {
-  Books: booksReducer,
-  Categories: categoriesReducer,
-};
-
 const store = configureStore({
-  reducers,
+  reducer: {
+    books: booksReducer,
+    category: categoriesReducer,
+  },
 });
 
 export default store;
